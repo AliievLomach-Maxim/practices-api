f9863466b8f80ce914b3700810c0424d707e6a63
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2023-06-29 01:53:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ const CSS_URL =
 
 // app.use(cors())
 // app.options('*', cors())
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+	next()
+})
 app.use(express.json())
 
 app.use(passport.initialize())
